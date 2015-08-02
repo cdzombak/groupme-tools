7928b6dfd0db382664cd0535b88cdd8c5d5123d2
Cory Zue <czue@dimagi.com>
2015-08-02 18:20:38 +0200

    write transcript to file instead of console

--- simple-transcript.py
+++ simple-transcript.py
@@ -6,58 +6,59 @@ import json
 import datetime
 
 
-def printTranscript(messages):
+def printTranscript(messages, outputFilename):
     """Prints a readable "transcript" from the given list of messages.
 
     Assumes the input list is sorted."""
+    with open(outputFilename, 'wb') as outFile:
+        for message in messages:
+            name = message[u'name']
+            time = datetime.datetime.fromtimestamp(message[u'created_at']).strftime('%Y-%m-%d %H:%M')
 
-    for message in messages:
-        name = message[u'name']
+            # text is None for a photo message
+            if message[u'text'] is not None:
+                text = message[u'text']
+            else:
+                text = "(no text)"
 
-        time = datetime.datetime.fromtimestamp(message[u'created_at']).strftime('%Y-%m-%d %H:%M')
+            if message[u'system'] is True:
+                system_padded = '(SYS) '
+            else:
+                system_padded = ''
 
-        # text is None for a photo message
-        if message[u'text'] is not None:
-            text = message[u'text']
-        else:
-            text = "(no text)"
+            if len(message[u'favorited_by']) is not 0:
+                favorites_padded = ' (' + str(len(message[u'favorited_by'])) + 'x <3)'
+            else:
+                favorites_padded = ''
 
-        if message[u'system'] is True:
-            system_padded = '(SYS) '
-        else:
-            system_padded = ''
+            if message[u'picture_url'] is not None:
+                pic = ' ; photo URL ' + message[u'picture_url']
+            else:
+                pic = ''
 
-        if len(message[u'favorited_by']) is not 0:
-            favorites_padded = ' (' + str(len(message[u'favorited_by'])) + 'x <3)'
-        else:
-            favorites_padded = ''
-
-        if message[u'picture_url'] is not None:
-            pic = ' ; photo URL ' + message[u'picture_url']
-        else:
-            pic = ''
-
-        string = system_padded + name + ' (' + time + ')' + favorites_padded + ': ' + text + pic
-        print(string.encode(sys.stdout.encoding, errors='replace'))
+            line = u'{0}{1}({2}){3}: {4}{5}\n'.format(
+                system_padded, name, time, favorites_padded, text, pic
+            )
+            outFile.write(line)
 
 
 def main():
-    """Usage: simple-transcript.py filename.json
+    """
+    Usage: simple-transcript.py transcript-filename.json output-filename.json
 
-Assumes filename.json is a JSON GroupMe transcript in chronological order.
+    Assumes filename.json is a JSON GroupMe transcript in chronological order.
 
-Times displayed in local timezone.
+    Times displayed in local timezone.
     """
 
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print(main.__doc__)
         sys.exit(1)
 
-    transcriptFile = open(sys.argv[1])
-    transcript = json.load(transcriptFile)
-    transcriptFile.close()
+    with open(sys.argv[1]) as transcriptFile:
+        transcript = json.load(transcriptFile)
 
-    printTranscript(transcript)
+    printTranscript(transcript, sys.argv[2])
 
 
 if __name__ == '__main__':
